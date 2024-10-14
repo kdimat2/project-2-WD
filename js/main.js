eda7b4b45df4dcd4ef232cf21cec7a2940fbb776
Katherine DiMattia <kdimat2@lsu.edu>
2024-10-13 22:35:00 -0500

    added thank you page and tried to add css idk man im trying

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // JS scripts placed here
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "flex";
-    }
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
   }
+}
